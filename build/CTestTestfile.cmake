73438fed1d60d1a84ebf2a55642fa643509abdc3
yizhuoyang <1274647873@qq.com>
2024-11-11 12:11:33 +0800

    update

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kemove/road_seg_2_cloud/src
-# Build directory: /home/kemove/road_seg_2_cloud/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("road_seg")
--- build/road_seg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kemove/road_seg_2_cloud/src/road_seg
-# Build directory: /home/kemove/road_seg_2_cloud/build/road_seg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
